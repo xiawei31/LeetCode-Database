f2a797725267838492a9fc97536f19978986411f
Winnie <xiawei31@users.noreply.github.com>
2016-04-28 02:18:00 -0400

    Create 184DepartmentHighestSalary.sql

---
+++ 184DepartmentHighestSalary.sql
@@ -0,0 +1,37 @@
+/*
+ The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+
+*/
+# Write your MySQL query statement below
+SELECT d.Name, e1.Name, e1.Salary
+FROM Employee e1 join
+(  select max(e2.Salary) MaxSalary, e2.DepartmentID
+  from Employee e2 group by e2.DepartmentID )  temp
+  on e1.Salary = temp.MaxSalary and e1.DepartmentID = temp.DepartmentID
+  join Department d on d.Id = temp.DepartmentID
